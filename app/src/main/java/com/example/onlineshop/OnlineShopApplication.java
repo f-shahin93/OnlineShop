387b74fb5ad385a77ae9d70b627e4d2dcb468f1e
SH <f.shahin.kh@gmail.com>
2020-01-22 14:42:11 +0330

    Add this class to create notification channel.

---
+++ app/src/main/java/com/example/onlineshop/OnlineShopApplication.java
@@ -0,0 +1,54 @@
+package com.example.onlineshop;
+
+import android.app.Application;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.onlineshop.event.NotificationEvent;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+public class OnlineShopApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannel();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+        EventBus.getDefault().unregister(this);
+    }
+
+    public void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            String id = getString(R.string.channel_id);
+            String name = getString(R.string.channel_name);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(id,name,importance);
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            manager.createNotificationChannel(channel);
+        }
+    }
+
+    @Subscribe(priority = 1)
+    public void onShowNotification(NotificationEvent event) {
+        Notification notification = event.getNotification();
+        int notificationId = event.getNotificationId();
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(notificationId, notification);
+    }
+
+}
